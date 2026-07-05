d206d06702050e934a377154d5a7cb2fc88b5c06
Avario Danvers <seanbillion1@gmail.com>
2026-07-05 08:32:41 +0700

    Fix NestJS dependency injection

--- backend/src/finance/finance.module.ts
+++ backend/src/finance/finance.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
 
+import { PrismaModule } from '../prisma/prisma.module';
+import { GlModule } from '../gl/gl.module';
+
 @Module({
+  imports: [PrismaModule, GlModule],
   controllers: [FinanceController],
   providers: [FinanceService]
 })
--- backend/src/gl/gl.module.ts
+++ backend/src/gl/gl.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GlService } from './gl.service';
 
+import { PrismaModule } from '../prisma/prisma.module';
+
 @Module({
-  providers: [GlService]
+  imports: [PrismaModule],
+  providers: [GlService],
+  exports: [GlService]
 })
 export class GlModule {}
